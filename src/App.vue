19e3ee217d6e6401d03f691e59933d3edf55900c
cbappi <abdulwahedrr@gmail.com>
2023-11-14 22:20:44 +0600

    function - method -es6

--- src/App.vue
+++ src/App.vue
@@ -5,8 +5,8 @@ import TheWelcome from './components/TheWelcome.vue'
 
 <template>
   <h1>{{staticEtring}}</h1><br>
-  <h1>Course Name :{{courseName}}</h1><br>
-  <h2>{{ info.name }} and {{ info.city }}</h2><br>
+  <h1 @click="changeCity">Course Name :{{courseName}}</h1><br>
+  <h2 @click="changeName">{{ info.name }} and {{ info.city }}</h2><br>
   <h3>{{ mentor }}</h3><br>
   <p><input type = "text" v-model="courseName"/></p>
   <p><input type = "number" v-model="count"/></p>
@@ -39,15 +39,22 @@ import TheWelcome from './components/TheWelcome.vue'
       )
 
       info.city = "Khulna";
+ //FUNCTION METHOD ES6 TYPE
+      const increment = amount => measure.value +=amount;
+      const decrement = amount => measure.value -=amount;
 
-      function increment(amount){
-           measure.value +=amount;
-      }
+      const changeName = () => info.name = "Rabbil Hasan";
+      const changeCity = () => info.city = "Rajshahi";
+   
+ //FUNCTION TYPE
+      // function increment(amount){
+      //      measure.value +=amount;
+      // }
 
       
-      function decrement(amount){
-           measure.value -=amount;
-      }
+      // function decrement(amount){
+      //      measure.value -=amount;
+      // }
      
 </script>
 
