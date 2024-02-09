959bb4e7203021e95a70387dcc81a032ed70df93
cbappi <abdulwahedrr@gmail.com>
2024-02-09 15:32:24 +0600

    Vue JS Function  commit

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,12 @@
 
 <template>
-  <h1>{{ title }}</h1><br>
-  <h2>{{ subject }}</h2><br>
-  <h2>{{ obj.name }}</h2><br>
-  <!-- Two way data binding -->
-  <input type = "tex" v-model="obj.name"/><br>
+  <h1>{{ obj.title }}</h1>
   <br>
+  <button @click = "increment">+</button>
   <br>
-  <input type = "number" v-model="count"/><br>
-  <h2>{{ obj.description }}</h2><br>
-  <button @click = "count++">{{ count }} </button>
+  <h2>{{ count }}</h2>
+  <br>
+  <button @click = "decrement(3)">-</button>
 </template>
 
 <!-- Script Set up -->
@@ -17,19 +14,21 @@
   import {ref, reactive} from 'vue';
 
       let count = ref(10);
-      
-      // if we declare count = 100, may dom will lost reactivity
-      // so count.value should use and reactivity regain
-      count.value = 100;
-      const title = ref('Programing journy');
-      const subject = ref('Vie JS Tutorial'); 
 
       let obj = reactive({
-        name: "Shorig",
+        title: "Vue JS Function",
         class: "Seven",
         location: "Chittagong",
         description: "Now Placing Object Property"
       })
+
+      function increment(){
+        count.value++;
+      }
+
+      function decrement(amount){
+        count.value -= amount;
+      }
 </script>
 
 
---
+++ src/components/twoway.vue
@@ -0,0 +1,33 @@
+
+<template>
+    <h1>{{ title }}</h1><br>
+    <h2>{{ subject }}</h2><br>
+    <h2>{{ obj.name }}</h2><br>
+    <!-- Two way data binding -->
+    <input type = "tex" v-model="obj.name"/><br>
+    <br>
+    <br>
+    <input type = "number" v-model="count"/><br>
+    <h2>{{ obj.description }}</h2><br>
+    <button @click = "count++">{{ count }} </button>
+  </template>
+  
+  <!-- Script Set up -->
+  <script setup>
+    import {ref, reactive} from 'vue';
+  
+        let count = ref(10);
+        
+        // if we declare count = 100, may dom will lost reactivity
+        // so count.value should use and reactivity regain
+        count.value = 100;
+        const title = ref('Programing journy');
+        const subject = ref('Vie JS Tutorial'); 
+  
+        let obj = reactive({
+          name: "Shorig",
+          class: "Seven",
+          location: "Chittagong",
+          description: "Now Placing Object Property"
+        })
+  </script>
