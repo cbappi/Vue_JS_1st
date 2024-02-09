2afc70fa6a96d5fd029f5ca0e175197dc4c88cf8
cbappi <abdulwahedrr@gmail.com>
2024-02-09 23:30:20 +0600

    Ditrctive V-If V-else V-else-if commit

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 
 <template>
 
-  <h1 v-show = "show">{{obj.title}}</h1>
+  <h1 v-if = "showBtn">{{obj.title}}</h1>
+  <h1 v-else-if = "!showBtn">{{showBtn}}</h1>
+  <h1 v-else = "showBtn">Nothing</h1>
   <br/>
-  <button @click = "show=!show">Show/Hide</button>
+  <button @click = "showBtn = !showBtn">Show/Hide</button>
   
 </template>
 
@@ -11,11 +13,11 @@
 <script setup>
 
   import {ref, reactive} from 'vue';  
-  let show = ref(true);
+  let showBtn = ref(false);
 
   
   let obj = reactive({
-        title: "Vue JS Function",
+        title: "Vue JS Function - V-if - V-else-if",
         class: "Seven"
      
       })
