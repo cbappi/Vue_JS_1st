8095e52ba25feabd341e9ff031aef860ed5f5e55
cbappi <abdulwahedrr@gmail.com>
2024-02-09 23:04:59 +0600

    Ditrctive V-ShoW commit

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,24 @@
 
 <template>
 
-  <!-- Example of computed method -->
-  <h1>Full name is :  {{ fullName }}</h1>
-  <br>
-  <!-- Example of computed method -->
-  <button @click = "increment">+</button>
-  <br>
-  <h2>{{ count }} / {{ numberChecker }}</h2>
-  <br>
-  <button @click = "decrement(3)">-</button>
+  <h1 v-show = "show">{{obj.title}}</h1>
+  <br/>
+  <button @click = "show=!show">Show/Hide</button>
+  
 </template>
 
-<!-- Script Set up -->
+
 <script setup>
-  import {ref, reactive, computed, watch} from 'vue';
 
-      let count = ref(10);
+  import {ref, reactive} from 'vue';  
+  let show = ref(true);
 
-      let obj = reactive({
+  
+  let obj = reactive({
         title: "Vue JS Function",
         class: "Seven"
      
       })
-
-      const fullName = computed(
-        ()=>{
-          return obj.title + '' + obj.class;
-        }
-      )
-
-      const increment = () =>{
-        count.value++;
-      }
-
-      const decrement = amount => {
-        count.value -= amount;
-      }
-
-      const numberChecker = computed(
-        ()=>{
-          if(count.value % 2==0){
-            return 'even number'
-          }
-          else{
-            return 'odd number'
-          }
-        }
-      )
-    //Here count.value is resource
-      watch(()=>count.value, (newValue, oldValue)=>{
-        if(newValue>17){
-          alert("No need to more")
-        }
-
-      })
-
 
 
 </script>
