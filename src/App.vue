6c13d5d68eed0fe4d15780cb803088dc45c2342b
cbappi <abdulwahedrr@gmail.com>
2024-02-13 21:16:32 +0600

    Binding Objects - HTML Class

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,10 @@
-
 <template>
 
   <h1>{{obj.title1}}</h1><br>
   <h1>{{obj.title2}}</h1><br>
 
 <div class = "static" 
-:style = "[isActive, hasError]">
+:class = "{'bg-black':isActive, 'text-danger':hasError}">
   This is test message
 </div>
 
@@ -13,18 +12,33 @@
 
 <script setup>
 
-  import {ref, reactive} from 'vue'; 
-
+  import {ref, reactive} from 'vue';  
+ const isActive = ref(true);   
+ const hasError = ref(false);    
   let obj = reactive({
         title1: "Section-13: Class and Style Binding",  
-        title2: "Binding Inline Styles using Array",  
+        title2: "Binding Objects - HTML Class",  
 
       })
-
-  const isActive = ref('font-size: 40px');   
-  const hasError = ref('color: black;background-color:orange'); 
 </script>
 
+<style>
+.bg-black{
 
+  background-color: black;
+  font-size: 30px;
+  color:white;
 
+}
 
+.text-danger{
+  background-color: black;
+  color:red;
+  font-size:30px;
+  font-style: italic;
+}
+
+.static{
+  margin-top:60px;
+}
+</style>
