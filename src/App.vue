8720c510032a57bc8775f4fe32e702f13a7c8e43
cbappi <abdulwahedrr@gmail.com>
2024-02-13 09:12:00 +0600

    Binding inline style with object

--- src/App.vue
+++ src/App.vue
@@ -4,41 +4,30 @@
   <h1>{{obj.title1}}</h1><br>
   <h1>{{obj.title2}}</h1><br>
 
-  
-<div :class = "isActive ? 'color-one':'color-two' " >
-  <h1>Ok</h1>
+<div class = "static" 
+:style = "inlineStyle">
+  This is test message
 </div>
 
- 
 </template>
 
 <script setup>
 
-  import {ref, reactive} from 'vue';  
+  import {reactive} from 'vue';  
 
-
-   let isActive = ref(true);
-   
- 
   let obj = reactive({
         title1: "Section-13: Class and Style Binding",  
-        title2: "Binding HTML Classes",  
+        title2: "Binding Objects - HTML Class",  
 
       })
-</script>
+  let inlineStyle = reactive({
+        color:'orange',  
+        fontSize:'60px',
+        backgroundColor:'black',  
 
-<style>
-.color-one{
-  width:100px;
-  height:100px;
-  background-color:aqua ;
-}
-.color-two{
-  width:100px;
-  height:100px;
-  background-color:brown;
-}
-</style>
+      })
+
+</script>
 
 
 
