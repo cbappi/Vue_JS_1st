525b34a674d6da5cec7ddb2c2d5f47e16b8a3e79
cbappi <abdulwahedrr@gmail.com>
2024-02-13 09:29:46 +0600

    Binding inline style using  array

--- src/App.vue
+++ src/App.vue
@@ -5,7 +5,7 @@
   <h1>{{obj.title2}}</h1><br>
 
 <div class = "static" 
-:style = "inlineStyle">
+:style = "[isActive, hasError]">
   This is test message
 </div>
 
@@ -13,20 +13,16 @@
 
 <script setup>
 
-  import {reactive} from 'vue';  
+  import {ref, reactive} from 'vue'; 
 
   let obj = reactive({
         title1: "Section-13: Class and Style Binding",  
-        title2: "Binding Objects - HTML Class",  
-
-      })
-  let inlineStyle = reactive({
-        color:'orange',  
-        fontSize:'60px',
-        backgroundColor:'black',  
+        title2: "Binding Inline Styles using Array",  
 
       })
 
+  const isActive = ref('font-size: 40px');   
+  const hasError = ref('color: black;background-color:orange'); 
 </script>
 
 
