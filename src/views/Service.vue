31c91001ddd53a7f9869be0682d8a97c750a3e60
cbappi <abdulwahedrr@gmail.com>
2024-02-14 00:23:57 +0600

    useRouter and router push

--- src/views/Service.vue
+++ src/views/Service.vue
@@ -2,6 +2,18 @@
 
 <div>
  <h1>This is service page</h1><br>
+ <button @click="gohome">Home</button>
 </div>
 
 </template>
+
+<script setup>
+  
+  import {useRouter} from 'vue-router'; 
+  const x = useRouter();
+  
+  const gohome = ()=>{
+    x.push({name:'home'})
+  }
+ 
+</script>
