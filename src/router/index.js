cf83f8191d35853802f07ac90b791901bcf2e5fa
cbappi <abdulwahedrr@gmail.com>
2024-02-14 00:02:21 +0600

    Named Route

--- src/router/index.js
+++ src/router/index.js
@@ -6,10 +6,10 @@ import Contact from '../views/Contact.vue';
 
 const routes = [
 
-    {path:'/', component:Home},
-    {path:'/about', component:About},
-    {path:'/service', component:Service},
-    {path:'/contact', component:Contact},
+    {path:'/', name:'home', component:Home},
+    {path:'/about-us', name:'about', component:About},
+    {path:'/service-hg', name:'service',  component:Service},
+    {path:'/contact', name:'contact',  component:Contact},
 
 ];
 
