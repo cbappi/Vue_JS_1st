931968f80829ff182ac16fbd2bde67c9182f0f1a
cbappi <abdulwahedrr@gmail.com>
2024-02-13 21:22:53 +0600

    Binding Array - HTML Class

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
   <h1>{{obj.title2}}</h1><br>
 
 <div class = "static" 
-:class = "{'bg-black':isActive, 'text-danger':hasError}">
+:class = [isActive,hasError]>
   This is test message
 </div>
 
@@ -13,28 +13,23 @@
 <script setup>
 
   import {ref, reactive} from 'vue';  
- const isActive = ref(true);   
- const hasError = ref(false);    
+ const isActive = ref('bg-black');   
+ const hasError = ref('text-danger');    
   let obj = reactive({
-        title1: "Section-13: Class and Style Binding",  
-        title2: "Binding Objects - HTML Class",  
+        title1: "Section-13:Class and Style Binding",  
+        title2: "Binding Array - HTML Class",  
 
       })
 </script>
 
 <style>
 .bg-black{
-
-  background-color: black;
-  font-size: 30px;
-  color:white;
-
+  font-size: 40px;
 }
 
 .text-danger{
   background-color: black;
   color:red;
-  font-size:30px;
   font-style: italic;
 }
 
