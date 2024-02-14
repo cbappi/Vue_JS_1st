1362c027ca91b910833094ea28cb49b2eed216e2
cbappi <abdulwahedrr@gmail.com>
2024-02-14 22:29:32 +0600

    Pinia state management

---
+++ src/store/service.js
@@ -0,0 +1,16 @@
+import { defineStore } from "pinia";
+
+export const useServiceStore = defineStore(
+    'service', {
+        state:()=>({
+            
+          services:[
+            {id:1, subject:"Web Design"},
+            {id:2, subject:"Graphic Design"},
+            {id:3, subject:"UI Design"},
+            {id:4, subject:"Seb Development"},
+            {id:5, subject:"Dev Ops"}
+        ]
+        })
+    }
+)
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,38 @@
 <template>
 
-    <h1>{{obj.title}}</h1><br>
+    <h1>{{counterStore.title}}</h1><br>
+    <button @click = "increment">+</button>
+    <br>
+    <h2>{{ counterStore.count }}</h2>
+    <br>
+    <button @click = "decrement">-</button>
+
+    <h1>Our Service</h1><br>
+
+    <ul>
+        <li v-for="service in serviceStore.services" :key="service.id">
+          <router-link :to = "{name:'single-service', params:{id:service.id}}">
+          {{ service.subject }}</router-link></li>
+    </ul>
+
 
 </template>
   
   <script setup>
   
-    import {reactive} from 'vue';  
+    import { useCounterStore } from '../store/counter';
+    import { useServiceStore } from '../store/service';
+
+    const counterStore = useCounterStore();
+    const serviceStore = useServiceStore();
    
-    let obj = reactive({
-          title: "This Home Page"
-        })
+    function increment(){
+      counterStore.count++;
+      }
+    function decrement(){
+      counterStore.count--;
+      }  
+    
   </script>
   
   
